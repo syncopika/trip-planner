4724ce33b195f150d64a11d2958d42497f176b47
nicholas hung <nhung93@outlook.com>
2020-12-30 23:15:39 -0500

    more tests!

--- tests/unit/calendar.spec.ts
+++ tests/unit/calendar.spec.ts
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, Wrapper } from '@vue/test-utils'
 import Calendar from '@/components/Calendar.vue'
 
 describe('Calendar.vue', () => {
@@ -20,6 +20,14 @@ describe('Calendar.vue', () => {
 	expect(wrapper.find(`#${destName}day`).exists()).toBe(true);
     expect(wrapper.find(`#${destName}month`).exists()).toBe(true);
 	expect(wrapper.find(`#${destName}year`).exists()).toBe(true);
+	
+	// make sure input text is empty
+	// TODO: this is correct because the input boxes shouldn't be populated with values until
+	// the updated lifecycle hook function is called. probably should separate the function and put in methods instead.
+	wrapper.findAll("input").wrappers.forEach((inputWrapper) => {
+		const inputEl : HTMLInputElement = (inputWrapper as Wrapper<Vue>).element as HTMLInputElement;
+		expect(inputEl.value).toBe("");
+	});
 	
   });
 })
