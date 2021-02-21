551ab59487bedcd7f36564ace5e7ab2a6ee33697
nicholas hung <nhung93@outlook.com>
2021-02-20 23:29:12 -0500

    making some progress on db stuff

--- api-server.js
+++ api-server.js
@@ -1,4 +1,5 @@
 const pg = require('pg');
+const express = require('express');
 
 // fill this info in with your local db info
 const client = new pg.Client({
@@ -26,4 +27,57 @@ client.query(query, (err, res) => {
 });
 */
 
+/*
+const query = `
+SELECT * FROM destinations
+`;
+client.query(query, (err, dbRes) => {
+	if(err){
+		console.log(err);
+	}
+	console.log(dbRes.rows);
+	client.end();
+});
+*/
+
+/*
+const query = `
+INSERT INTO destinations (username, destname, tripname, index, latitude, longitude, metadata)
+VALUES ('test_user', 'test_dest', 'test_trip', 0, 25.0785476, 121.2326428, '{}')
+`;
+client.query(query, (err, res) => {
+	if(err){
+		console.log(err);
+	}else{
+		console.log('insert successful!');
+	}
+	client.end();
+});
+*/
+
 // TODO: set up app to host api endpoints to be called for interacting with the db
+const app = express();
+
+app.get("/", (req, res) => {
+	res.json({message: "hello there"});
+});
+
+// probably should be POST
+app.get("/destinations", (req, res) => {
+	// do the db lookup
+	const query = `
+	SELECT * FROM destinations
+	`;
+	client.query(query, (err, dbRes) => {
+		if(err){
+			console.log(err);
+		}else{
+			res.json({data: dbRes.rows});
+		}
+		client.end();
+	});
+});
+
+app.listen(8081, () => {
+	console.log("server running on port 8081...");
+});
