1dd82c1f554870f884f8be730ade573cbe145e04
nicholas hung <nhung93@outlook.com>
2023-11-20 14:40:05 -0500

    more testing

--- tests/unit/options_modal.spec.ts
+++ tests/unit/options_modal.spec.ts
@@ -16,7 +16,13 @@ describe('OptionsModal.vue', () => {
         expect(wrapper.findAll("h1").length).toBe(1);
         expect(wrapper.find("h1").text()).toEqual("options");
 	
-        // TODO: make sure expected elements are disabled if applicable + have the right values based on props
+        // make sure expected elements are disabled if applicable + have the right values based on props
+        expect(wrapper.find("#themeSelect").exists()).toBe(true);
+        expect(wrapper.findAll("button").length).toBe(2); // save and close buttons
+        expect((wrapper.find("#toggleSuggestedDestinations").element as HTMLInputElement).checked).toEqual(false);
+        expect((wrapper.find("#toggleLocationSearchBar").element as HTMLInputElement).checked).toEqual(false);
+        expect((wrapper.find("#databaseOption").element as HTMLInputElement).disabled).toEqual(true);
+        expect((wrapper.find("#overpassApiOption").element as HTMLInputElement).disabled).toEqual(true);
     });
   
 })
